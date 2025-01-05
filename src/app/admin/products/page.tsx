a5a428239c96aaa044c39237af2a491380657b7f
Yaswanth Mallisetti <mallisettiyaswanth@gmail.com>
2025-01-05 07:56:47 +0530

    data-table-demo

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import { Metadata } from "next";
+import fs from "fs";
+import path from "path";
+import { DataTable } from "@/components/global/data-table/data-table";
+import { columns } from "@/components/global/data-table/columns";
 
-type Props = {};
-
-const page = (props: Props) => {
-  return (
-    <div className="p-5">
-      <div className="w-full flex items-center justify-between">
-        <h1 className="text-3xl">Products list</h1>
-      </div>
-    </div>
-  );
+export const metadata: Metadata = {
+  title: "Expenses",
+  description: "A Expense tracker build using Tanstack Table.",
 };
 
-export default page;
+async function getData() {
+  const filePath = path.join(
+    process.cwd(),
+    "src/app/admin/products",
+    "data.json"
+  );
+  const data = fs.readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+export default async function Page() {
+  const data = await getData();
+  console.log("data", data);
+
+  return (
+    <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
+      <div className="flex items-center justify-between">
+        <p className="text-muted-foreground">
+          Here&apos;s a list of your expenses for this month!
+        </p>
+      </div>
+      <DataTable data={data} columns={columns} />
+    </div>
+  );
+}
